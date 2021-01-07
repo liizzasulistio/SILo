0d02a812b930f5dba47fc872c2032a7de856e047
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-01-07 10:03:46 +0700

    create class Users

---
+++ WebContent/header.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <title>${param.title}</title>
+ 	<%--  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<title>${param.title}</title> --%>
+</head>
+<body>
+
+
---
+++ src/silo/Users.java
@@ -0,0 +1,31 @@
+package silo;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Users 
+{
+	private int UserID;
+	private String UserName;
+	private String UserType;
+	private String UserEmail;
+	private String UserPassword;
+
+	public int getUserID() { return UserID; }
+	public void setUserID(int UserID) { this.UserID = UserID; }
+	
+	public String getUserName() { return UserName; }
+	public void setUserName(String UserName) { this.UserName = UserName; }
+	
+	public String getUserType() { return UserType; }
+	public void setUserType(String UserType) { this.UserType = UserType; }
+	
+	public String getUserEmail() { return UserEmail; }
+	public void setUserEmail(String UserEmail) { this.UserEmail = UserEmail; }
+	
+	public String getUserPassword() { return UserPassword; }
+	public void setUserPassword(String UserPassword) { this.UserPassword = UserPassword; }
+}
